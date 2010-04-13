ee72563281f320c5628f567c563df7ca79f73837
Daemon <daemonaps@hotmail.fr>
2010-04-13 10:13:12 +0200

    Cleanup dans Vue/Template.php : retours lignes Unix

--- Vue/Template.php
+++ Vue/Template.php
@@ -1,1 +1,33 @@
-<?phpif(!defined('_IN'))	exit;function AddTemplateCustomVars($obj, $isCached){	if($isCached)		return;		// Ajouter ici les items de template personalises.	// (...)		// Ajouter en dessous les items de template, en fonction de la page	// (Si ils sont presents sur plusieurs pages, sinon, les definir sur la page)	/** Exemple :	switch($obj->page)	{		case 'index':		case 'page2':		{						$obj->setVar('total_account', AccountMgr::GetCount());					}		default:		{					}		}	//**/}?>
+<?php
+
+if(!defined('_IN'))
+	exit;
+
+function AddTemplateCustomVars($obj, $isCached)
+{
+	if($isCached)
+		return;
+	
+	// Ajouter ici les items de template personalises.
+	// (...)
+	
+	// Ajouter en dessous les items de template, en fonction de la page
+	// (Si ils sont presents sur plusieurs pages, sinon, les definir sur la page)
+	/** Exemple :
+	switch($obj->page)
+	{
+		case 'index':
+		case 'page2':
+		{
+			
+			$obj->setVar('total_account', AccountMgr::GetCount());
+			
+		}
+		default:
+		{
+			
+		}	
+	}
+	//**/
+}
+?>
